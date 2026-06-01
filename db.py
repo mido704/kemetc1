445744daaf360357549e5c1318e5f165cdcb95e9
mido704 <mido704@gmail.com>
2026-06-01 08:04:26 +0300

    Correct return statement in db.py
    Fix return statement for PostgreSQL connection.

--- db.py
+++ db.py
@@ -279,7 +279,6 @@ def get_conn():
         )
        conn.autocommit = False
         return PGConnWrapper(conn)
-        return conn
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     conn.execute("PRAGMA foreign_keys = ON")
