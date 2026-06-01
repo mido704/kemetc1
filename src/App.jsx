83b6d9f944f0c2beebefdbe85bb498065d7351ab
mido704 <mido704@gmail.com>
2026-06-01 15:10:05 +0300

    Add url prop to Avatar component for image support

--- src/App.jsx
+++ src/App.jsx
@@ -153,10 +153,10 @@ function Toast({ msg, onDone }) {
   return <div className="toast">{msg}</div>;
 }
 
-function Avatar({ emoji = '👑', size = 44, onClick }) {
+function Avatar({ emoji = '👑', size = 44, onClick, url }) {
   return (
-    <div className="av" style={{ width: size, height: size, fontSize: size * 0.45 }} onClick={onClick}>
-      {emoji}
+    <div className="av" style={{ width: size, height: size, fontSize: size * 0.45, overflow:'hidden', padding: url ? 0 : undefined }} onClick={onClick}>
+      {url ? <img src={url} style={{width:'100%',height:'100%',objectFit:'cover',borderRadius:'50%'}} /> : emoji}
     </div>
   );
 }
