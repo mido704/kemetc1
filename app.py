a2032849fca1c507b8dab9f522d19c338a338cf1
mido704 <mido704@gmail.com>
2026-06-10 18:09:00 +0300

    fix: add missing columns migration for tours table

--- app.py
+++ app.py
@@ -395,11 +395,17 @@ with app.app_context():
         conn.close()
     except:
         pass
+    try:
+        conn2 = init_db()
     conn = init_db()
     seed_data(conn)
     conn.close()
 @app.route('/api/admin/stats', methods=['GET'])
 @require_auth
+        conn2.commit()
+        conn2.close()
+    except:
+        pass
 def admin_stats():
     uid = request.current_user['id']
     # Check if admin
